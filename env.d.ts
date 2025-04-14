a71f815895320881320229c8d02f5af1c2eca27d
Gabriel Jordan <gabrijordan3@gmail.com>
2025-04-14 14:43:11 -0300

    Adicionar arquivos de configuração .env e tipos de ambiente para o projeto

---
+++ env.d.ts
@@ -0,0 +1,13 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      PORT?: string;
+
+      DB_HOST: string;
+      DB_PORT: string;
+      DB_USERNAME: string;
+      DB_PASSWORD: string;
+      DB_DATABASE: string;
+    }
+  }
+}
