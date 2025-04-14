f149f6793fca0c2951625bc450f24cfd82892531
Gabriel Jordan <gabrijordan3@gmail.com>
2025-04-14 14:49:30 -0300

    configurar conexão com banco de dados postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from './modules/health/health.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { databaseConfig } from './config/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [HealthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: databaseConfig,
+    }),
+    HealthModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/config/database.config.ts
@@ -0,0 +1,17 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+export const databaseConfig = (
+  configService: ConfigService,
+): TypeOrmModuleOptions => ({
+  type: 'postgres',
+  host: configService.get('DATABASE_HOST'),
+  port: parseInt(configService.get('DATABASE_PORT') || '5432'),
+  username: configService.get('DATABASE_USER'),
+  password: configService.get('DATABASE_PASSWORD'),
+  database: configService.get('DATABASE_NAME'),
+  entities: [__dirname + '/../modules/**/entities/*.entity{.ts,.js}'],
+
+  // Ativado temporariamente em desenvolvimento, não considero boa prática deixar isso ativado
+  synchronize: true,
+});
